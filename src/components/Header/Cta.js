f60499a2cf929805591aba78ed4f27399e392036
mohamed98 <mohamedatta447@gmail.com>
2023-07-02 01:21:19 +0300

    header outline

--- src/components/Header/Cta.js
+++ src/components/Header/Cta.js
@@ -1,7 +1,17 @@
+import { PlusIcon } from '@heroicons/react/20/solid';
+
 export const Cta = () => {
 	return (
 		<div>
-			<button>Cta</button>
+			<div className="flex-shrink-0">
+				<button
+					type="button"
+					className="relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+				>
+					<PlusIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
+					New Job
+				</button>
+			</div>
 		</div>
 	);
 };
--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,9 +1,16 @@
 export const Logo = () => {
 	return (
-		<div>
-			<a className="text-3xl font-bold underline" href="/">
-				Logo
-			</a>
+		<div className="flex flex-shrink-0 items-center">
+			<img
+				className="block h-8 w-auto lg:hidden"
+				src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+				alt="Your Company"
+			/>
+			<img
+				className="hidden h-8 w-auto lg:block"
+				src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+				alt="Your Company"
+			/>
 		</div>
 	);
 };
